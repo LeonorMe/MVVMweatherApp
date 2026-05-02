ae00f49b51b509f6b1d1b9a0ff7195e7b235f4bb
Francisco Parra <pf8976@gmail.com>
2026-05-02 13:29:50 +0100

    fix WeatherAPIClient

--- app/src/main/java/dam_a50758/mvvmweatherapp/data/WeatherAPIClient.kt
+++ app/src/main/java/dam_a50758/mvvmweatherapp/data/WeatherAPIClient.kt
@@ -1,35 +1,21 @@
-// TODO ktor ContentNegotiation json example
-// kotlin buildString example
-// kotlin suspend function explained
-
 
 object WeatherApiClient{
-    private val Client = HttpClient {
+
+    private val client = HttpClient {
         install(ContentNegotiation){
             json(Json{
                 ignoreUnknownKeys = true
             })
         }
     }
-}
 
-fun main(){
-    val lat = 38.0
-    val lon = -9.4
-    
-    getWeather(lat, lon)
-    WeatherApiClient.getWeather(lat, lon)
-}
+    suspend fun getWeather(lat: Float, lon: Float): WeatherData?{
+        val url = "https://api.open-meteo.com/v1/forecast?latitude=$lat&longitude=$lon&daily=uv_index_max&hourly=pressure_msl&current=wind_speed_10m,wind_direction_10m,temperature_2m,weather_code,is_day&timezone=Europe%2FLondon&forecast_days=3"
 
-
-suspend fun getWeather(lat: float, lon: Float): WeatherData?{
-    val url = "https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&daily=uv_index_max&hourly=pressure_msl&current=wind_speed_10m,wind_direction_10m,temperature_2m,weather_code,is_day&timezone=Europe%2FLondon&forecast_days=3"
-
-    try{
-        client.get(url).body<WeatherData>()
-    } catch (e: Exception){
-
+        try{
+            return client.get(url).body<WeatherData>()
+        } catch (e: Exception){
+            return null
+        }
     }
-    
 }
-
