e403aed73ac2187fef798199ba27cdf482a1aec2
Francisco Parra <pf8976@gmail.com>
2026-05-02 15:12:06 +0100

    fix weather view model

--- app/src/main/java/dam_a50758/mvvmweatherapp/viewmodel/WeatherViewModel.kt
+++ app/src/main/java/dam_a50758/mvvmweatherapp/viewmodel/WeatherViewModel.kt
@@ -2,37 +2,46 @@ package dam_a50758.mvvmweatherapp.viewmodel
 
 class WeatherViewModel(
 ): ViewModel(){
-    private val _uistate = MutableStateFlow(WeatherUIState(
-        0f, 0f, 0f, 0f, 0, 0f, "", 0, 0f
+    private val _uiState = MutableStateFlow(WeatherUIState(
+        0f, 0f, 0f, 0f, 0, 0, 0f, "", 0, 0f
     ))
     
-    val uiState: StateFlow<WeatherUIState> = _uistate
+    val uiState: StateFlow<WeatherUIState> = _uiState.asStateFlow()
 
     fun updateLatitude(newLat: Float){
-
+        _uiState.value = _uiState.value.copy(latitude = newLat)
     }
 
     fun updateLongitude(newLon: Float){
-        state.copy()
+        _uiState.value = _uiState.value.copy(longitude = newLon)
     }
 
     fun fetchWeather(){
         // get current lat/lon from state
-        lat = state.
-        lon = state.
-
-        // call WeatherApiClient.getWeather(lat, lon)
-        // convert API data in UI state
-        // update _uiState
-        // getWeather() is suspend and ViewModel needs a coroutine
-        viewModelScope.launch{
-            _uiState = WeatherApiClient.getWeather(lat, lon)
-            
-            // data mapping
-            temperature = weather.current.temperature_2m
-            _uiState.value = _uiState.value.copy()
-        }
+        val lat = _uiState.value.latitude
+        val lon = _uiState.value.longitude
 
         
+        // convert API data in UI state
+        // getWeather() is suspend and ViewModel needs a coroutine
+        viewModelScope.launch{
+            // call WeatherApiClient.getWeather(lat, lon)
+            val weather = WeatherApiClient.getWeather(lat, lon)
+
+            if (weather != null){
+                // update _uiState
+                _uiState.value = _uiState.value.copy(
+                    // data mapping
+                    temperature = weather.current.temperature_2m,
+                    windSpeed: weather.current.wind_speed_10m,
+                    windDirection: weather.current.wind_direction_10m,
+                    weatherCode: weather.current.weatherCode,
+                    seaLevelPressure: weather.hourly.pressure_msl[0],
+                    time: weather.current.time,
+                    isDay: weather.current.is_day,
+                    uvIndexMax: weather.daily.uv_index_max[0]
+                )
+            }  
+        }
     }
 }
