1ccc636378d83399c7da0996ab02d92f088b5388
Francisco Parra <pf8976@gmail.com>
2026-05-02 16:20:07 +0100

    improved WeatherScreen

--- app/src/main/java/dam_a50758/mvvmweatherapp/ui/WeatherScreen.kt
+++ app/src/main/java/dam_a50758/mvvmweatherapp/ui/WeatherScreen.kt
@@ -2,36 +2,16 @@ package dam_a50758.mvvmweatherapp.uiState
 
 import androidx.compose.runtime.*
 import androidx.compose.ui.text.input.TextFieldValue
-/* 
+
 @Composable
 fun WeatherContent(
     uiState: WeatherUiState,
+    latField: TextFieldValue
+    lonField: TextFieldValue
     onLatitudeChange: (Float) -> Unit,
     onLongitudeChange: (Float) -> Unit,
     onFetch: () -> Unit
-)
-*/
-@Composable 
-fun WeatherUI(
-    weatherViewModel: WeatherViewModel = viewModel()
 ){
-
-    val uiState by weatherViewModel.uiState.collectAsState()
-
-    //var latField by remember {mutableStateOf(TextFieldValue(uiState.latitude.toString))}
-    //var lonField by remember {mutableStateOf(TextFieldValue(uiState.longitude.toString))}
-
-    val latitude = uiState.latitude 
-    val longitude = uiState.longitude
-    val temperature = uiState.temperature
-    val windSpeed = uiState.windSpeed
-    val windDirection = uiState.windDirection
-    val weatherCode = uiState.weatherCode
-    val seaLevelPressure = uiState.seaLevelPressure
-    val time = uiState.time
-    val isDay = uiState.isDay
-    val uvIndexMax = uiState.uvIndexMax
-
     Column{
         TextFieldValue(
             value = latitude.toString(),
@@ -68,4 +48,33 @@ fun WeatherUI(
             Text("Update")
         }
     }
+}
+
+
+@Composable 
+fun WeatherUI(
+    weatherViewModel: WeatherViewModel = viewModel()
+){
+
+    val uiState by weatherViewModel.uiState.collectAsState()
+
+    var latField by remember {mutableStateOf(TextFieldValue(uiState.latitude.toString))}
+    var lonField by remember {mutableStateOf(TextFieldValue(uiState.longitude.toString))}
+
+    WeatherContent(
+        uiState = uiState,
+        latField = latField,
+        lonField = lonField,
+        onLatChange = {
+            latField = it
+            it.text.toFloatOrNull()?.let(weatherViewModel::updateLatitude)
+        },
+        onLongitudeChange = {
+            lonField = it
+            it.text.toFloatOrNull()?.let(weatherViewModel::updateLongitude)
+        },
+        onFetch = {
+            weatherViewModel.fetchWeather()
+        }
+    )
 }
